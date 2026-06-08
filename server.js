146b4c2962e1acf16e8616afbc46b9554a056672
DovzenkoYaroslav <159412926+DovzenkoYaroslav@users.noreply.github.com>
2026-06-08 19:01:42 +0300

    Bump to 2.0.0

--- server.js
+++ server.js
@@ -83,9 +83,14 @@ function syncPlayerToGlobal(player) {
   };
 }
 
+const APP_VERSION = JSON.parse(fs.readFileSync(path.join(__dirname, 'package.json'), 'utf8')).version || '0.0.0';
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+app.get('/version', (req, res) => {
+  res.json({ version: APP_VERSION });
 });
 
 // --- ИГРОВОЙ ДВИЖОК ---
